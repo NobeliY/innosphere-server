aae9352b37318b2dd5ab734ba98d927dd6548418
Repaer@Zet <nobeliylord@gmail.com>
2024-08-08 12:32:26 +0300

    UPD: Log4js configuration

--- index.js
+++ index.js
@@ -10,9 +10,34 @@ const https = require('https')
 const fs = require('fs')
 var log4js = require('log4js');
 log4js.configure({
-    appenders: [{type: 'console'},
-                {type: 'file', filename: 'express.log', category: 'dev'}]
-});
+    "appenders": {
+      "access": {
+        "type": "dateFile",
+        "filename": "log/access.log",
+        "pattern": "-yyyy-MM-dd",
+        "category": "http"
+      },
+      "app": {
+        "type": "file",
+        "filename": "log/app.log",
+        "maxLogSize": 10485760,
+        "numBackups": 3
+      },
+      "errorFile": {
+        "type": "file",
+        "filename": "log/errors.log"
+      },
+      "errors": {
+        "type": "logLevelFilter",
+        "level": "ERROR",
+        "appender": "errorFile"
+      }
+    },
+    "categories": {
+      "default": { "appenders": [ "app", "errors" ], "level": "DEBUG" },
+      "http": { "appenders": [ "access"], "level": "DEBUG" }
+    }
+  });
 
 var logger = log4js.getLogger('dev');
 logger.setLevel('DEBUG');
