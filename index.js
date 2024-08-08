1b29d6bff3d61621bf07ec032ab8453d2a5bd7a9
Repaer@Zet <nobeliylord@gmail.com>
2024-08-08 12:19:52 +0300

    UPD: add log4js

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ const router = require('./routes/index')
 const path = require('path')
 const https = require('https')
 const fs = require('fs')
+var log4js = require('log4js');
+log4js.configure({
+    appenders: [{type: 'console'},
+                {type: 'file', filename: 'express.log', category: 'dev'}]
+});
 
+var logger = log4js.getLogger('dev');
+logger.setLevel('DEBUG');
 
 const PORT = process.env.PORT || 5000
 
 //const options = {}
 // for linux
-
 const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/server.innosfera-almet.ru/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/server.innosfera-almet.ru/cert.pem')
@@ -26,6 +32,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
+app.use(log4js.connectLogger(logger, {level: log4js.levels.DEBUG}));
 app.use('/api', router)
 
 // for linux
